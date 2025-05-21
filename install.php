e4f8f436205bad42a3638178ac0c4b414ccc40e0
MarJose123 <18107626+MarJose123@users.noreply.github.com>
2025-05-21 09:16:53 +0800

    Initial Commit

---
+++ install.php
@@ -0,0 +1,78 @@
+#!/usr/bin/env php
+<?php
+function ask(string $question, string $default = ''): string
+{
+    $answer = readline($question.($default ? " ({$default})" : null).': ');
+
+    if (! $answer) {
+        return $default;
+    }
+
+    return $answer;
+}
+
+function ask_password(string $question): string
+{
+    shell_exec('stty -echo');
+    $pass = trim(fgets(STDIN));
+    shell_exec('stty echo');
+    return $pass;
+}
+function confirm(string $question, bool $default = false): bool
+{
+    $answer = ask($question.' ('.($default ? 'Y/n' : 'y/N').')');
+
+    if (! $answer) {
+        return $default;
+    }
+
+    return strtolower($answer) === 'y';
+}
+
+function writeln(string $line): void
+{
+    echo $line.PHP_EOL;
+}
+
+function run(string $command): string
+{
+    return trim((string) shell_exec($command));
+}
+
+function str_after(string $subject, string $search): string
+{
+    $pos = strrpos($subject, $search);
+
+    if ($pos === false) {
+        return $subject;
+    }
+
+    return substr($subject, $pos + strlen($search));
+}
+
+function slugify(string $subject): string
+{
+    return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $subject), '-'));
+}
+
+function replace_in_file(string $file, array $replacements): void
+{
+    $contents = file_get_contents($file);
+
+    file_put_contents(
+        $file,
+        str_replace(
+            array_keys($replacements),
+            array_values($replacements),
+            $contents
+        )
+    );
+}
+
+
+function determineSeparator(string $path): string
+{
+    return str_replace('/', DIRECTORY_SEPARATOR, $path);
+}
+
+
